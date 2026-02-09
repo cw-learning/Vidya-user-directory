093c61f9b32302fc30b68ba7dcdbb6c23dbda19e
vidya-cw <vidya@codewalnutlabs.com>
2026-02-09 22:53:04 +0530

    feat: initial project setup with React Vite, Tailwind, Biome, Vitest and RTL

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold text-center mt-10">
+				athlete-checkin-dashboard
+			</h1>
+		</div>
+	);
+}
+
+export default App;
